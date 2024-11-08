ea72f4588a9e791ec330d3d13f40c7b24da4f02a
MinYJ <mesutjune@gmail.com>
2024-11-09 07:29:20 +0900

    optimize SEO

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,18 @@
-import { StrictMode } from 'react'
+import { lazy, StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import NotFound from './pages/NotFound.jsx'
-import Videos from './pages/Videos.jsx'
-import VideoDetail from './pages/VideoDetail.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { HelmetProvider } from 'react-helmet-async'
+
+/* code_splitting_before */
+import VideoDetail from './pages/VideoDetail.jsx'
+import Videos from './pages/Videos.jsx'
+
+/* code_splitting_after */
+// const VideoDetail = lazy(() => import('./pages/VideoDetail.jsx'));
+// const Videos = lazy(() => import('./pages/Videos.jsx'));
 
 const router = createBrowserRouter([
   {
@@ -22,6 +29,15 @@ const router = createBrowserRouter([
   }
 ])
 createRoot(document.getElementById('root')).render(
+  /* code_splitting_after */
+  // <Suspense>
+  //   <RouterProvider router={router} />
+  // </Suspense> 
+
+
+  /* code_splitting_before */
+  <HelmetProvider>
     <RouterProvider router={router} />
+  </HelmetProvider>
 
 )
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import prerender from "@prerenderer/rollup-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // prerender({
+    //   routes: ["/", "/videos", "/videos/watch/"],
+    //   renderer: "@prerenderer/renderer-puppeteer",
+    //   server: {
+    //     port: 3000,
+    //     host: "localhost",
+    //   },
+    //   rendererOptions: {
+    //     maxConcurrentRoutes: 1,
+    //     renderAfterTime: 500,
+    //   },
+    //   postProcess(renderedRoute) {
+    //     renderedRoute.html = renderedRoute.html
+    //       .replace(/http:/i, "https:")
+    //       .replace(
+    //         /(https:\/\/)?(localhost|127\.0\.0\.1):\d*/i,
+    //         "https://tubular-cat-f2a272.netlify.app"
+    //       );
+    //   },
+    // })
+  ],
   base: "/",
 })
